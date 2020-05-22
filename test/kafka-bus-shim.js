0c5d759e117b8cdeb5f562a4ccb2beaa63b25d34
Patrick Lee Scott <pat@patscott.io>
2020-05-22 00:57:15 -0400

    feat: exactly once delivery (transactions)
    BREAKING CHANGE: This adds a transactional producer which will enable exactly once delivery. This results in more reliability and less speed. This is now the default for `send` and `publish`. It's still possible to use faster producer via options `{ transaction: false }` with less deliverability.

--- test/kafka-bus-shim.js
+++ test/kafka-bus-shim.js
@@ -1,5 +1,5 @@
-require('should');
-require('longjohn');
+require('should')
+require('longjohn')
 let log = require('debug')('servicebus')
 let { connectionOpts } = require('../testHelpers')
 
@@ -7,24 +7,24 @@ let { connectionOpts } = require('../testHelpers')
 //   throw new Error('Tests require a KAFKA_HOSTS environment variable to be set, pointing to the Kafka instance you wish to use.');
 
 // var brokers = process.env.KAFKA_HOSTS;
-var retry = require('@servicebus/retry');
+var retry = require('@servicebus/retry')
 
 let kafkaBus = require('../').bus
 
 var store = new retry.RedisStore({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT
-});
+})
 
 module.exports = async function () {
-  let bus 
-  
+  let bus
+
   try {
     bus = await kafkaBus({
       ...connectionOpts(),
-      brokers: ['localhost:9093', 'localhost:9095', 'localhost:9097'],
-      serviceName: 'test',
-    });
+      brokers: ['localhost:9092', 'localhost:9095', 'localhost:9098'],
+      serviceName: 'test'
+    })
   } catch (e) {
     throw e
   }
@@ -34,15 +34,17 @@ module.exports = async function () {
   log(bus)
 
   // bus.use(bus.messageDomain());
-  bus.use(bus.package());
-  bus.use(bus.correlate());
+  bus.use(bus.package())
+  bus.use(bus.correlate())
   // bus.use(bus.logger());
-  bus.use(retry({
-    setRetriesRemaining: true,
-    store,
-    keyExpireTTL: 0,
-    namespace: 'test'
-  }))
+  bus.use(
+    retry({
+      setRetriesRemaining: true,
+      store,
+      keyExpireTTL: 0,
+      namespace: 'test'
+    })
+  )
 
   return bus
-};
+}
--- test/kafka-send-listen.js
+++ test/kafka-send-listen.js
@@ -1,22 +1,20 @@
-const log = require('debug')('servicebus:test');
-const kafkabus = require('./kafka-bus-shim');
+const log = require('debug')('servicebus:test')
+const kafkabus = require('./kafka-bus-shim')
 
-describe('kafka servicebus', function(){
-
-  describe('#send & #listen', function() {
-
-    it('should cause message to be received by listen', async function (){
+describe('kafka servicebus', function () {
+  describe('#send & #listen', function () {
+    it('should cause message to be received by listen', async function () {
       return new Promise(async (resolve, reject) => {
         let bus = await kafkabus()
-        this.timeout(30000);
+        this.timeout(30000)
         log('bus.listen', bus.listen)
         let listener = await bus.listen('my.event.1', function (event) {
           console.log('event data', event.data)
-          resolve(true);
-        });
-        await bus.send('my.event.1', { my: 'event' });
-      })      
-    });
+          resolve(true)
+        })
+        await bus.send('my.event.1', { my: 'event' })
+      })
+    })
 
     // TODO: each could be created with different consumer group - but is that really what we want?
     // messages ARE NOT distributed in the same way as rmq so I don't think this really makes sense
@@ -62,43 +60,45 @@ describe('kafka servicebus', function(){
     //   })
     // });
 
-    it('can handle high event throughput', async function (){
+    it('can handle high event throughput when using batch produce', async function () {
       return new Promise(async (resolve, reject) => {
         let bus = await kafkabus()
         let time = 30000
-        this.timeout(time);
+        this.timeout(time)
         setTimeout(() => {
           console.log(`processed ${count} messages`)
         }, time - 100)
-        var count = 0, batchSize = 2000, repeatBatch = 5;
-        function tryDone(){
-          count++;
+        var count = 0,
+          batchSize = 2000,
+          repeatBatch = 5
+        function tryDone () {
+          count++
           if (count >= batchSize * repeatBatch) {
             console.log(`processed ${count} messages`)
-            resolve();
+            resolve()
           }
         }
 
         await bus.listen('my.command.3', function (event) {
-          tryDone();
-        });
+          tryDone()
+        })
 
-        var i = 0;
+        var i = 0
         var messages = []
-        for(var i = 0; i < batchSize; ++i) {
+        for (var i = 0; i < batchSize; ++i) {
           messages.push({ my: 'event' })
         }
 
-        for(var r = 0; r < repeatBatch; ++r) {
+        for (var r = 0; r < repeatBatch; ++r) {
           log('sending batch', r)
           await bus.produceBatch({
             topic: 'my.command.3',
             messages,
             messageType: 'command'
-          });
+          })
         }
       })
-    });
+    })
 
     // it.only('can handle higher event throughput with batch produce', async function (){
     //   return new Promise(async (resolve, reject) => {
@@ -156,7 +156,7 @@ describe('kafka servicebus', function(){
     //   })
     // });
 
-    // TODO: enableConfirms not implemented 
+    // TODO: enableConfirms not implemented
     // it.skip('should use callback in confirm mode', function (done) {
     //   confirmBus.send('my.event.19', { my: 'event' }, {}, function (err, ok) {
     //     done(err);
@@ -182,66 +182,69 @@ describe('kafka servicebus', function(){
         let bus = await kafkabus()
         await bus.listen('my.event.25', { ack: true }, function (msg, message) {
           log('msg')
-          log(msg) 
+          log(msg)
           log('message')
           log(message)
 
           msg.handle.ack()
-          
+
           resolve()
-        });
+        })
         setTimeout(async function () {
-          await bus.send('my.event.25', { payload: 'test' }, { ack: true, correlationId: 'test-id' });
-        }, 100);
+          await bus.send(
+            'my.event.25',
+            { payload: 'test' },
+            { ack: true, correlationId: 'test-id' }
+          )
+        }, 100)
       })
-    });
+    })
+  })
 
-  });
+  //   describe('#unlisten', function() {
 
-//   describe('#unlisten', function() {
+  //     it('should cause message to not be received by listen', function (done){
+  //       var completed = false;
+  //       function tryDone () {
+  //         if (completed) return true;
+  //         completed = true;
+  //         done();
+  //       }
+  //       bus.listen('my.event.17', function (event) {
+  //         tryDone(new Error('should not receive events after unlisten'));
+  //       });
+  //       setTimeout(function () {
+  //         bus.unlisten('my.event.17').on('success', function () {
+  //           bus.send('my.event.17', { test: 'data'});
+  //           setTimeout(function () {
+  //             tryDone();
+  //           }, 100);
+  //         });
+  //       }, 1500);
+  //     });
 
-//     it('should cause message to not be received by listen', function (done){
-//       var completed = false;
-//       function tryDone () {
-//         if (completed) return true;
-//         completed = true;
-//         done();
-//       }
-//       bus.listen('my.event.17', function (event) {
-//         tryDone(new Error('should not receive events after unlisten'));
-//       });
-//       setTimeout(function () {
-//         bus.unlisten('my.event.17').on('success', function () {
-//           bus.send('my.event.17', { test: 'data'});
-//           setTimeout(function () {
-//             tryDone();
-//           }, 100);
-//         });
-//       }, 1500);
-//     });
+  //   });
 
-//   });
+  //   describe('#destroyListener', function() {
 
-//   describe('#destroyListener', function() {
+  //     it('should cause message to not be received by listen', function (done){
+  //       var completed = false;
+  //       function tryDone () {
+  //         if (completed) return true;
+  //         completed = true;
+  //         done();
+  //       }
+  //       bus.listen('my.event.18', { ack: true }, function (event) {
+  //         event.handle.ack();
+  //         tryDone(new Error('should not receive events after destroy'));
+  //       });
+  //       // setTimeout(function () {
+  //         bus.destroyListener('my.event.18').on('success', function () {
+  //           bus.send('my.event.18', { test: 'data'}, { ack: true, expiration: 100 });
+  //           setTimeout(tryDone, 100);
+  //         });
+  //       // }, 1500);
+  //     });
 
-//     it('should cause message to not be received by listen', function (done){
-//       var completed = false;
-//       function tryDone () {
-//         if (completed) return true;
-//         completed = true;
-//         done();
-//       }
-//       bus.listen('my.event.18', { ack: true }, function (event) {
-//         event.handle.ack();
-//         tryDone(new Error('should not receive events after destroy'));
-//       });
-//       // setTimeout(function () {
-//         bus.destroyListener('my.event.18').on('success', function () {
-//           bus.send('my.event.18', { test: 'data'}, { ack: true, expiration: 100 });
-//           setTimeout(tryDone, 100);
-//         });
-//       // }, 1500);
-//     });
-
-//   });
-});
+  //   });
+})
